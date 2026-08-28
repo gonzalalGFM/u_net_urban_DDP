6c7f57d69a32cc5afcaa8331ca12a6e7c1e816eb
gonzalalGFM <169052757+gonzalalGFM@users.noreply.github.com>
2026-08-28 12:15:06 +0200

    Update train_ddp_workers_benchmark.py

--- src/python_scripts/train_ddp_workers_benchmark.py
+++ src/python_scripts/train_ddp_workers_benchmark.py
@@ -16,8 +16,8 @@ from torchmetrics.functional.image import structural_similarity_index_measure
 os.environ.setdefault("HDF5_USE_FILE_LOCKING", "FALSE")
 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent / "unet_testing"))
-from dataset import CFDDataset  # noqa: E402
-from model import UNet  # noqa: E402
+from dataset import CFDDataset  
+from model import UNet  
 
 OUTPUT_CHANNELS = [0, 1, 2, 3]
 N_BLOCKS = 3
